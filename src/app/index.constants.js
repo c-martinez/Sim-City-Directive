29868c3c5e2959669c0c53b3ab721b4417241fdc
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2016-05-31 13:26:09 +0200

    Removed malarkey and moment constants

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -1,10 +1,7 @@
-/* global malarkey:false, moment:false */
 (function() {
   'use strict';
 
   angular
-    .module('simCitySimDirective')
-    .constant('malarkey', malarkey)
-    .constant('moment', moment);
+    .module('simCitySimDirective');
 
 })();
